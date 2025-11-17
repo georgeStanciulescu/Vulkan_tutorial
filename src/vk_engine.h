fbd0844eaea359e26b3b669d08bf68874d8edcda
George Stanciulescu <georgestanciulescu0@gmail.com>
2025-11-17 18:21:59 +0000

    Following a vulkan tutorial

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -4,16 +4,120 @@
 #pragma once
 
 #include <vk_types.h>
+#include <vk_loader.h>
+
+#include "vk_descriptors.h"
+
+struct ComputePushConstants {
+    glm::vec4 data1;
+    glm::vec4 data2;
+    glm::vec4 data3;
+    glm::vec4 data4;
+};
+
+struct ComputeEffect {
+    const char* name;
+
+    VkPipeline pipeline;
+    VkPipelineLayout layout;
+
+    ComputePushConstants data;
+};
+
+struct DeletionQueue
+{
+    std::deque<std::function<void()>> deletors;
+
+    void push_function(std::function<void()>&& function) {
+        deletors.push_back(function);
+    }
+
+    void flush() {
+        // reverse iterate the deletion queue to execute all the functions
+        for (auto it = deletors.rbegin(); it != deletors.rend(); it++) {
+            (*it)(); //call functors
+        }
+
+        deletors.clear();
+    }
+};
+
+struct FrameData {
+    VkCommandPool _commandPool;
+    VkCommandBuffer _mainCommandBuffer;
+    VkSemaphore _swapchainSemaphore, _renderSemaphore;
+    VkFence _renderFence;
+
+    DeletionQueue _deletionQueue;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
 
 class VulkanEngine {
 public:
 
+    std::vector<std::shared_ptr<MeshAsset>> testMeshes;
+
+    VkFence _immFence;
+    VkCommandBuffer _immCommandBuffer;
+    VkCommandPool _immCommandPool;
+
+    VkPipeline _gradientPipeline;
+    VkPipelineLayout _gradientPipelineLayout;
+
+    // VkPipelineLayout _trianglePipelineLayout;
+    // VkPipeline _trianglePipeline;
+
+    VkPipelineLayout _meshPipelineLayout;
+    VkPipeline _meshPipeline;
+
+    GPUMeshBuffers rectangle;
+
+    DescriptorAllocator globalDescriptorAllocator;
+
+    VkDescriptorSet _drawImageDescriptors;
+    VkDescriptorSetLayout _drawImageDescriptorLayout;
+
+    AllocatedImage _drawImage;
+    VkExtent2D _drawExtent;
+
+    AllocatedImage _depthImage;
+
+    VmaAllocator _allocator;
+
+    DeletionQueue _mainDeletionQueue;
+
+    VkInstance _instance;// Vulkan library handle
+    VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+    VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+    VkDevice _device; // Vulkan device for commands
+    VkSurfaceKHR _surface;// Vulkan window surface
+
+    VkSwapchainKHR _swapchain;
+    VkFormat _swapchainImageFormat;
+
+    std::vector<VkImage> _swapchainImages;
+    std::vector<VkImageView> _swapchainImageViews;
+    VkExtent2D _swapchainExtent;
+
+    FrameData _frames[FRAME_OVERLAP];
+
+    FrameData& get_current_frame(){return _frames[_frameNumber % FRAME_OVERLAP];}
+
+    VkQueue _graphicsQueue;
+    uint32_t _graphicsQueueFamily;
+
 	bool _isInitialized{ false };
 	int _frameNumber {0};
-
+	bool stop_rendering{ false };
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
+    std::vector<ComputeEffect> backgroundEffects;
+    int currentBackgroundEffect{0};
+
 	struct SDL_Window* _window{ nullptr };
+
+	static VulkanEngine& Get();
 
 	//initializes everything in the engine
 	void init();
@@ -26,4 +130,33 @@ public:
 
 	//run main loop
 	void run();
+
+    void immediate_submit(std::function<void(VkCommandBuffer cmd)>&& function);
+
+    AllocatedBuffer create_buffer(size_t allocSize, VkBufferUsageFlags usage, VmaMemoryUsage memoryUsage);
+    void destroy_buffer(const AllocatedBuffer& buffer);
+
+    GPUMeshBuffers uploadMesh(std::span<uint32_t> indices, std::span<Vertex> vertices);
+
+    void init_mesh_pipeline();
+
+private:
+
+    void init_vulkan();
+    void init_swapchain();
+    void init_commands();
+    void init_sync_structures();
+    void init_descriptors();
+    void init_pipelines();
+    void init_background_pipelines();
+    void init_imgui();
+    //void init_triangle_pipeline();
+    void init_default_data();
+
+    void draw_imgui(VkCommandBuffer cmd, VkImageView targetImageView);
+    void draw_background(VkCommandBuffer cmd);
+    void draw_geometry(VkCommandBuffer cmd);
+
+    void create_swapchain(uint32_t width, uint32_t height);
+    void destroy_swapchain();
 };
--- src/vk_types.h
+++ src/vk_types.h
@@ -1,8 +1,68 @@
 ﻿// vulkan_guide.h : Include file for standard system include files,
 // or project specific include files.
-
 #pragma once
 
-#include <vulkan/vulkan.h>
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+#include <span>
+#include <array>
+#include <functional>
+#include <deque>
 
-//we will add our main reusable types here
+#include <vulkan/vulkan.h>
+#include <vulkan/vk_enum_string_helper.h>
+#include <vk_mem_alloc.h>
+
+#include <fmt/core.h>
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+
+struct AllocatedImage {
+    VkImage image;
+    VkImageView imageView;
+    VmaAllocation allocation;
+    VkExtent3D imageExtent;
+    VkFormat imageFormat;
+};
+
+struct AllocatedBuffer {
+    VkBuffer buffer;
+    VmaAllocation allocation;
+    VmaAllocationInfo info;
+};
+
+struct Vertex {
+
+    glm::vec3 position;
+    float uv_x;
+    glm::vec3 normal;
+    float uv_y;
+    glm::vec4 color;
+};
+
+// holds the resources needed for a mesh
+struct GPUMeshBuffers {
+
+    AllocatedBuffer indexBuffer;
+    AllocatedBuffer vertexBuffer;
+    VkDeviceAddress vertexBufferAddress;
+};
+
+// push constants for our mesh object draws
+struct GPUDrawPushConstants {
+    glm::mat4 worldMatrix;
+    VkDeviceAddress vertexBuffer;
+};
+
+
+#define VK_CHECK(x)                                                     \
+    do {                                                                \
+        VkResult err = x;                                               \
+        if (err) {                                                      \
+            fmt::print("Detected Vulkan error: {} \n", string_VkResult(err)); \
+            abort();                                                    \
+        }                                                               \
+    } while (0)
